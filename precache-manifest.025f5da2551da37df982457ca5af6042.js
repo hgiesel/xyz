8ae3757a6aeaa85eba0450594185ac5cd71da394
Henrik Giesel <hengiesel@gmail.com>
2020-09-17 23:29:40 +0200

    Updates

---
+++ precache-manifest.025f5da2551da37df982457ca5af6042.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a0dda3edab981628910218a53d63ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef4f4b0805b4a9dda1b9",
+    "url": "/static/css/main.23117e0c.chunk.css"
+  },
+  {
+    "revision": "dc6280e849884a13ca09",
+    "url": "/static/js/2.b8b21291.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b8b21291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef4f4b0805b4a9dda1b9",
+    "url": "/static/js/main.aaf0b7f3.chunk.js"
+  },
+  {
+    "revision": "047d1f39cb1bec9579eb",
+    "url": "/static/js/runtime-main.791e4208.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
